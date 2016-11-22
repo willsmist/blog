d345174e65ba0adf24f4044015eb9f63056a8877
yourdaisy <yourdaisy@163.com>
2016-11-22 22:43:37 +0800

    add new post

---
+++ hexo-deploy&git-push.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+cd ~/Workspaces/blog
+
+# push files to github
+git add .
+git commit -m "add new post"
+git push origin master
+
+# deploy site
+hexo cl && hexo g && hexo d && hexo cl
